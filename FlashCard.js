0748aa0c312310c195fdbb5b6fba0bc5370270f3
jheal006 <jheal006@gmail.com>
2017-09-18 20:25:29 -0700

    Everything but the else statement seems to work

--- FlashCard.js
+++ FlashCard.js
@@ -3,40 +3,35 @@ var cards = require('./cards.json');
 var ClozeCard = require('./ClozeCard.js');
 var lineBreak = "==========================================";
 
-//Array of questions
-
-// var operation = new ClozeCard("Barbarossa was the German code name given to the invasion of the Soviet Union during World War II", "Barbarossa");
-//
-// var crusade = new ClozeCard("Lionheart was the nickname given to King Richard I of England", "Lionheart");
-
-//Display front portion of flashcard
-
+// Counter variable to step through Cards array
+var i = 0;
 
 function runFlashCards(){
-  // Counter variable to step through Cards array
-  var i = 0;
   // Pass card array through Cloze Card Constructor
   var question = new ClozeCard(cards[i].text, cards[i].cloze);
   // console.log(question);
-
-  inquirer.prompt([
-      {
-        name: "input",
-        message: question.partial + "\nAnswer: "
-      }
-    ]).then(function(answer) {
-        if (answer.input == question.cloze) {
-          //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-          return console.log("\n YES that is Correct\n" + lineBreak);
-        } else {
-        //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-          return console.log("\n No, the correct answer is: " + question.cloze + "\n" + lineBreak);
-          console.log("User Input ====== " + answer.input);
-          console.log("Answer.cloze ====== " + question.cloze);
+  if ( i < 5 ) {
+    inquirer.prompt([
+        {
+          name: "input",
+          message: question.partial + "\nAnswer: "
         }
-        // Increase counter variable
-        i++;
-    });
+      ]).then(function(answer) {
+          if (answer.input == question.cloze) {
+            //If user Input is Correct / Matches Cloze value, display Yes and the full text.
+             console.log("\n YES that is Correct\n" + lineBreak);
+          } else {
+          //If user Input is Correct / Matches Cloze value, display Yes and the full text.
+             console.log("\n No, the correct answer is: " + question.cloze + "\n" + lineBreak);
+          }
+          // Increase counter variable
+          i++;
+          runFlashCards();
+      });
+  } else if ( i = 5 ) {
+    console.log('Better crack open the history books!');
+  }
+
 };
 
 runFlashCards();
