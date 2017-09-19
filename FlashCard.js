4f2f63dd4c724f1304a9f0d5199b97eeaadeb7d3
jheal006 <jheal006@gmail.com>
2017-09-18 20:03:50 -0700

    Implemented clozeCard constructor along with json data array, now need to step through questions

--- FlashCard.js
+++ FlashCard.js
@@ -5,32 +5,38 @@ var lineBreak = "==========================================";
 
 //Array of questions
 
-var operation = new ClozeCard("Barbarossa was the German code name given to the invasion of the Soviet Union during World War II", "Barbarossa");
-
-var crusade = new ClozeCard("Lionheart was the nickname given to King Richard I of England", "Lionheart");
+// var operation = new ClozeCard("Barbarossa was the German code name given to the invasion of the Soviet Union during World War II", "Barbarossa");
+//
+// var crusade = new ClozeCard("Lionheart was the nickname given to King Richard I of England", "Lionheart");
 
 //Display front portion of flashcard
 
 
 function runFlashCards(){
   // Counter variable to step through Cards array
-  var i = 1;
+  var i = 0;
   // Pass card array through Cloze Card Constructor
-  var question = new ClozeCard(cards[i]);
+  var question = new ClozeCard(cards[i].text, cards[i].cloze);
+  // console.log(question);
+
   inquirer.prompt([
-        {
-          name: "input",
-          message: cards[i].text + "\nAnswer: "
-        }
-      ]).then(function(answer) {
-          if (answer.input == cards[i].cloze) {
-            //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-            return console.log("\n YES that is Correct\n" + lineBreak);
-          } else {
+      {
+        name: "input",
+        message: question.partial + "\nAnswer: "
+      }
+    ]).then(function(answer) {
+        if (answer.input == question.cloze) {
           //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-            return console.log("\n No, the correct answer is: " + operation.cloze + "\n" + lineBreak);
-            console.log("User Input ====== " + answer.input);
-            console.log("Answer.cloze ====== " + cards[i].cloze);
-          }
-      });
+          return console.log("\n YES that is Correct\n" + lineBreak);
+        } else {
+        //If user Input is Correct / Matches Cloze value, display Yes and the full text.
+          return console.log("\n No, the correct answer is: " + question.cloze + "\n" + lineBreak);
+          console.log("User Input ====== " + answer.input);
+          console.log("Answer.cloze ====== " + question.cloze);
+        }
+        // Increase counter variable
+        i++;
+    });
 };
+
+runFlashCards();
