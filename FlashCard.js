427bbfda7d194ab2aa7b88a8a2e2e433c276c96e
jheal006 <jheal006@gmail.com>
2017-09-18 19:45:43 -0700

    Still needing to pass the questions from the cards.json file through the clozeCard constructor function

--- FlashCard.js
+++ FlashCard.js
@@ -1,20 +1,9 @@
 var inquirer = require('inquirer');
+var cards = require('./cards.json');
 var ClozeCard = require('./ClozeCard.js');
-
-//Display Question
-var question = process.argv[2];
-var guess  = process.argv[3];
-
-var lineBreak = "=========================";
-
+var lineBreak = "==========================================";
 
 //Array of questions
-
-var newQuestions = [
-  ["Barbarossa was the German code name given to the invasion of the Soviet Union", "Barbarossa"],
-  ["George Washington was the name of the first President of the United States", "George Washington"]
-]
-
 
 var operation = new ClozeCard("Barbarossa was the German code name given to the invasion of the Soviet Union during World War II", "Barbarossa");
 
@@ -22,46 +11,26 @@ var crusade = new ClozeCard("Lionheart was the nickname given to King Richard I 
 
 //Display front portion of flashcard
 
-inquirer.prompt([
-  {
-    name: "Operation",
-    message: operation.partial + "\n Answer: ",
-    validate: function(answer){
-      //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-      if (answer == operation.cloze){
-        return console.log("\n YES that is Correct\n--------");
-      } else {
-      //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-        return console.log("\n No, the correct answer is : \n---------" + operation.cloze);
-      }
-    }
-  },
 
-  {
-    name: "Crusade",
-    message: crusade.partial + "\n Answer: ",
-    validate: function(answer){
-      //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-      if (answer === crusade.cloze){
-        return console.log("\n YES that is Correct \n================")
-      } else {
-      //If user Input is Correct / Matches Cloze value, display Yes and the full text.
-        return console.log('\n No, the correct answer is:' + crusade.cloze)
-      }
-    }
-  },
-
-
-]);
-
-
-
-
-//If user Input is Incorrect / Does not match CLoze value, display No and reveal the Cloze text and full text.
-
-
-
-//
-// var operation = new ClozeCard(newQuestions[0][0],newQuestions[0][1]);
-//
-// console.log(operation.text);
+function runFlashCards(){
+  // Counter variable to step through Cards array
+  var i = 1;
+  // Pass card array through Cloze Card Constructor
+  var question = new ClozeCard(cards[i]);
+  inquirer.prompt([
+        {
+          name: "input",
+          message: cards[i].text + "\nAnswer: "
+        }
+      ]).then(function(answer) {
+          if (answer.input == cards[i].cloze) {
+            //If user Input is Correct / Matches Cloze value, display Yes and the full text.
+            return console.log("\n YES that is Correct\n" + lineBreak);
+          } else {
+          //If user Input is Correct / Matches Cloze value, display Yes and the full text.
+            return console.log("\n No, the correct answer is: " + operation.cloze + "\n" + lineBreak);
+            console.log("User Input ====== " + answer.input);
+            console.log("Answer.cloze ====== " + cards[i].cloze);
+          }
+      });
+};
